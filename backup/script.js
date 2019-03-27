c9ac46af929715f7ec811feb47ae6cbaf52a3f5e
VadimKalayda <vadosgta1@gmail.com>
2019-03-27 19:31:52 +0200

    animations from css to js

---
+++ backup/script.js
@@ -0,0 +1,5 @@
+function firstMessage() {
+  document.querySelector('#sign-up').classList.toggle('active');
+  document.querySelector('#login-in').classList.toggle('active');
+  document.querySelector('#login_form').classList.toggle('active');
+}
--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+document.querySelector('#sign-up').classList.toggle('active2');
+document.querySelector('#login-in').classList.toggle('active3');
+document.querySelector('#login_form').classList.toggle('active');
+
+var active1 = document.querySelector('.active'),
+  active2 = document.querySelector('.active2'),
+  active3 = document.querySelector('.active3');
+
 function firstMessage() {
-  document.querySelector('#sign-up').classList.toggle('active');
-  document.querySelector('#login-in').classList.toggle('active');
-  document.querySelector('#login_form').classList.toggle('active');
+  active1.style.minHeight = '355px';
+
+  active2.style.top = '0';
+  active2.style.animationName = 'opacity1';
+  active2.style.animationDuration = '0.7s';
+  active2.style.opacity = '1';
+  active2.style.zIndex = '1';
+
+  active3.style.top = '60px';
+  active3.style.animationName = 'opacity2';
+  active3.style.animationDuration = '0.7s';
+  active3.style.opacity = '0';
+  active3.style.zIndex = '-100';
+}
+
+function secondMessage() {
+  active1.style.minHeight = '260px';
+
+  active2.style.top = '60px';
+  active2.style.animationName = 'opacity2';
+  active2.style.animationDuration = '0.0001s';
+  active2.style.opacity = '0';
+  active2.style.zIndex = '-100';
+
+  active3.style.top = '0';
+  active3.style.animationName = 'opacity2';
+  active3.style.animationDuration = '0.7s';
+  active3.style.opacity = '1';
+  active3.style.zIndex = '100';
 }
